26f192fd8f2b28b9c9696bef1402770340d208fa
thirdformant <r01daw13@abdn.ac.uk>
2018-08-12 14:04:19 +0100

    Initial commit
    Inserts data from csv into db using pandas and sqlite3

---
+++ data/db/db_insertion.py
@@ -0,0 +1,39 @@
+import sqlite3
+import pandas as pd
+from db_helpers import create_connection
+
+
+def main():
+    database = "./data/db/lego_db_test.db"
+    PATH = "./data/"
+
+    conn = create_connection(database)
+    if conn is not None:
+        # Read all data from csv files
+        print(f"Reading in raw data from {PATH}...")
+
+        sets_csv = pd.read_csv(f"{PATH}raw/sets.csv")
+        themes_csv = pd.read_csv(f"{PATH}raw/themes.csv")
+        inventories_csv = pd.read_csv(f"{PATH}raw/inventories.csv")
+        parts_csv = pd.read_csv(f"{PATH}raw/parts.csv")
+        colors_csv = pd.read_csv(f"{PATH}raw/colors.csv")
+        inventory_sets_csv = pd.read_csv(f"{PATH}raw/inventory_sets.csv")
+        inventory_parts_csv = pd.read_csv(f"{PATH}raw/inventory_parts.csv")
+
+        # Insert data into db
+        print("Inserting data into db...")
+
+        sets_csv.to_sql("sets", conn, if_exists='replace', index=False)
+        themes_csv.to_sql("themes", conn, if_exists='replace', index=False)
+        inventories_csv.to_sql("inventories", conn, if_exists="replace", index=False)
+        parts_csv.to_sql("parts", conn, if_exists="replace", index=False)
+        colors_csv.to_sql("colors", conn, if_exists="replace", index=False)
+        inventory_sets_csv.to_sql("inventory_sets", conn, if_exists="replace", index=False)
+        inventory_parts_csv.to_sql("inventory_parts", conn, if_exists="replace", index=False)
+
+    else:
+        print("Cannot create the database connection")
+
+
+if __name__ == '__main__':
+    main()
