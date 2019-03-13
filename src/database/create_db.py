ad9a352f4f8016311c1a03ee135348b7a47a4739
thirdformant <r01daw13@abdn.ac.uk>
2019-03-13 14:25:49 +0000

    Add db schema and table creator

--- src/database/create_db.py
+++ src/database/create_db.py
@@ -47,7 +47,7 @@ def parse_arguments()->dict:
 #==================
 # DB funcs
 #==================
-def create_connection(db):
+def create_connection(db:str)->sqlite3.connect:
     """ Creates connection to specified database file
     :param db: location of db file
     :return: DB connection object or none
@@ -58,6 +58,85 @@ def create_connection(db):
         return conn
     except Exception as e:
         LOGGER.exception(e)
+
+
+def create_table(conn:sqlite.conn, sql_create_table:str):
+    """ Creates database table from to SQL statement
+    :param conn: db connection object
+    :param sql_create_table: CREATE TABLE SQL statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(sql_create_table)
+    except Exception as e:
+        LOGGER.exception(e)
+
+
+def make_database(output:str):
+    database = output_dir
+
+    sets_table = """CREATE TABLE sets (
+        set_num TEXT PRIMARY KEY,
+        name TEXT,
+        year INTEGER,
+        theme_id INTEGER REFERENCES themes(id),
+        num_parts INTEGER
+    );"""
+
+    themes_table = """CREATE TABLE themes (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        parent_id INTEGER
+    );"""
+
+    inventories_table = """CREATE TABLE inventories (
+        id INTEGER PRIMARY KEY,
+        version INTEGER,
+        set_num INTEGER REFERENCES sets(set_num)
+    );"""
+
+    parts_table = """CREATE TABLE parts (
+        part_num TEXT PRIMARY KEY,
+        part_name TEXT,
+        part_cat_id INTEGER REFERENCES part_categories(id)
+    );"""
+
+    categories_table = """CREATE TABLE part_categories (
+        id INTEGER PRIMARY KEY,
+        name TEXT
+    );"""
+
+    colors_table = """CREATE TABLE colors (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        rgb TEXT,
+        is_trans BOOLEAN
+    );"""
+
+    invpart_table = """CREATE TABLE inventory_parts (
+        inventory_id INTEGER PRIMARY KEY REFERENCES inventories(id),
+        part_num TEXT REFERENCES parts(part_num),
+        color_id INTEGER REFERENCES colors(id),
+        quantity INTEGER,
+        is_spare BOOLEAN
+    );"""
+
+    invsets_table = """CREATE TABLE inventory_sets (
+        inventory_id INTEGER PRIMARY KEY REFERENCES inventories(id),
+        set_num TEXT REFERENCES sets(set_num),
+        quantity INTEGER
+    );"""
+
+    conn = create_connection(database)
+    create_table(conn, sets_table)
+    create_table(conn, themes_table)
+    create_table(conn, inventories_table)
+    create_table(conn, parts_table)
+    create_table(conn, categories_table)
+    create_table(conn, colors_table)
+    create_table(conn, invpart_table)
+    create_table(conn, invsets_table)
 
 
 #==================
