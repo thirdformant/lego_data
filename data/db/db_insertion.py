8b1b3e118515a84f531805282c0bcad5f007ab20
thirdformant <r01daw13@abdn.ac.uk>
2019-03-13 10:12:15 +0000

    Move connection func to insertion script

--- data/db/db_insertion.py
+++ data/db/db_insertion.py
@@ -1,6 +1,18 @@
 import sqlite3
 import pandas as pd
-from db_helpers import create_connection
+
+
+def create_connection(db):
+    """ Creates connection to specified database file
+    :param db: location of db file
+    :return: DB connection object or none
+    """
+    try:
+        conn = sqlite3.connect(db)
+        print("hmm")
+        return conn
+    except Exception as e:
+        print(e)
 
 
 def main():
